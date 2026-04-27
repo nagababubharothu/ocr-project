34ec42f1816a0468e2e7937c83d280ec61927e2b
nagababubharothu <nagababu.23bce20231@vitapstudent.ac.in>
2026-04-28 00:16:10 +0530

    final working OCR project

--- routes/ocrRoutes.js
+++ routes/ocrRoutes.js
@@ -4,19 +4,29 @@ const Tesseract = require("tesseract.js");
 
 const router = express.Router();
 
-// store uploaded files
-const upload = multer({ dest: "uploads/" });
+// Only allow images
+const upload = multer({
+    dest: "uploads/",
+    fileFilter: (req, file, cb) => {
+        if (
+            file.mimetype === "image/jpeg" ||
+            file.mimetype === "image/png"
+        ) {
+            cb(null, true);
+        } else {
+            cb(new Error("Only JPG/PNG images allowed"), false);
+        }
+    }
+});
 
+// OCR API
 router.post("/extract", upload.single("file"), async (req, res) => {
     try {
         if (!req.file) {
-            return res.status(400).json({ error: "No file uploaded" });
+            return res.status(400).json({ error: "No image uploaded" });
         }
 
-        const result = await Tesseract.recognize(
-            req.file.path,
-            "eng"
-        );
+        const result = await Tesseract.recognize(req.file.path, "eng");
 
         res.json({
             text: result.data.text
@@ -24,7 +34,7 @@ router.post("/extract", upload.single("file"), async (req, res) => {
 
     } catch (err) {
         res.status(500).json({
-            error: "OCR failed"
+            error: err.message
         });
     }
 });
